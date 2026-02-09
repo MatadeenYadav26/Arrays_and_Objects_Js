a78cc885464bed51b047eb8b1f628d46fb07ed57
KING <matadeenyadav202@gmail.com>
2026-02-09 10:05:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+// Arrays:
+// let arr = [1,2,3,4,5,6]
+// console.log(arr[2]);
+
+// How to create Array:
+// 1. let arr = [1,2,3,4,5]
+// 2.let arr = new Array(1,2,3,4,5,9,8);
+
+// functions in array: 
+// let arr = [1,2,3,4,5,7]
+// arr.push(99)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.unshift(0)
+// console.log(arr)
+// // arr.indexOf(4)
+// let[a,,b] = arr;
+// console.log(a)
+// console.log(b)
+// let arr2 = arr.filter(function(val){
+//     return val<3;
+// })
+// console.log(arr2);
+// console.log(arr);
+// let arr3 = [...arr];
+// console.log(...arr);
+
+// Itterating over Arrays using : forloop and forEach:
+// 1. For-loop:
+// let arr = [1,2,3,4,5]
+// for(i=0;i<4;i++){
+//     console.log(arr[i]);
+// }
+
+// 2. forEach 
+// let arr  = [1,2,3,4,5]
+// arr.forEach(function(val){
+//     console.log(val);
+// })
+
+// objects
+// let obj = {
+//     name : "Aman",
+//     Age : 21,
+//     mail : "mail@gmail.com",
+// };
+// console.log(obj);
+
+// let obj2 = new Object();
+
+// Accessing Object:-
+
+// let obj = {
+//     name : "Aman"
+// }
+// console.log(obj["name"])
+// console.log(obj.name)
+
+// Deleting proprty:
+
+// let obj = {
+//     name : "Aman"
+// }
+// delete obj.name;
+// console.log(obj);
+
+// Nested Object: 
+
+// let obj = {
+//     name: "Aman",
+//     socials : {
+//         insta : "meow",
+//         facebook : "woof"
+//     }
+// }
+// console.log(obj);  
